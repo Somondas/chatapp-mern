0acdc1e544e924f94b6047f5c479d5f168a6343d
Somondas <somondas067@gmail.com>
2024-04-16 22:11:30 +0530

    Updates

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,31 @@
-import { Container, Paper } from "@mui/material";
-import React from "react";
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { Camera as CameraAltIcon } from "@mui/icons-material";
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  // ? setIsLogin((prev => !prev)), this will make the previous value of isLogin to be toggled. Eg: true will became false and false will became true
+  const toggleLogin = () => setIsLogin((prev) => !prev);
   return (
-    <Container component={"main"} maxWidth="sx">
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Paper
         elevation={3}
         sx={{
@@ -12,7 +34,126 @@ const Login = () => {
           flexDirection: "column",
           alignItems: "center",
         }}
-      ></Paper>
+      >
+        {isLogin ? (
+          // >> Login Form
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                fullWidth
+                variant="contained"
+                type="submit"
+                color="primary"
+              >
+                Login
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                Or
+              </Typography>
+              <Button fullWidth variant="text" onClick={toggleLogin}>
+                Sign Up Instead
+              </Button>
+            </form>
+          </>
+        ) : (
+          // >> Sign In Form
+          <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                <Avatar
+                  sx={{
+                    width: "10rem",
+                    height: "10rem",
+                    objectFit: "contain",
+                  }}
+                />
+                <IconButton>
+                  <>
+                    <CameraAltIcon />
+                    <VisuallyHidden>Upload Avatar</VisuallyHidden>
+                  </>
+                </IconButton>
+              </Stack>
+              <TextField
+                required
+                fullWidth
+                label="Name"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Bio"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                fullWidth
+                variant="contained"
+                type="submit"
+                color="primary"
+              >
+                Sign Up
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                Or
+              </Typography>
+              <Button fullWidth variant="text" onClick={toggleLogin}>
+                Login Instead
+              </Button>
+            </form>
+          </>
+        )}
+      </Paper>
     </Container>
   );
 };
