8ccaf3a6f8be41a41eb245c7910d320edcb51184
Somondas <somondas067@gmail.com>
2024-04-18 18:15:39 +0530

    created protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute.jsx";
 
 const Home = lazy(() => import("./pages/Home.jsx"));
 const Login = lazy(() => import("./pages/Login.jsx"));
@@ -7,12 +8,34 @@ const Chat = lazy(() => import("./pages/Chat.jsx"));
 const Groups = lazy(() => import("./pages/Groups.jsx"));
 
 const App = () => {
+  const user = true;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
+        <Route
+          path="/"
+          element={
+            <ProtectRoute user={user}>
+              <Home />
+            </ProtectRoute>
+          }
+        />
+        <Route
+          path="/chat/:chatId"
+          element={
+            <ProtectRoute user={user}>
+              <Chat />
+            </ProtectRoute>
+          }
+        />
+        <Route
+          path="/groups"
+          element={
+            <ProtectRoute user={user}>
+              <Groups />
+            </ProtectRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
