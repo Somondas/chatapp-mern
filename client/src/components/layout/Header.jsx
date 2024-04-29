1a3266256145ecd760a4dab3a9a2fba72bf85b0e
Somondas <somondas067@gmail.com>
2024-04-29 17:37:03 +0530

    Header Done

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,7 +1,106 @@
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React from "react";
-
+import { orange } from "../../constants/color";
+import {
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  Group as GroupIcon,
+  Logout as LogoutIcon,
+  Add as AddIcon,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+// |__________________________________________________________
 const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  // >> Handling Functions------------------------------
+  const handleMobile = () => {
+    console.log("handleMobile");
+  };
+  const openSearchDialog = () => {
+    console.log("openSearchDialog");
+  };
+  const openNewGroup = () => {
+    console.log("openNewGroup");
+  };
+  const navigateToGroup = () => {
+    navigate("/groups");
+  };
+  const logoutHandler = () => {
+    console.log("logoutHandler");
+  };
+  // >>----------------------------------------------------
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: orange,
+          }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Chattu
+            </Typography>
+            <Box
+              sx={{
+                display: { xs: "block", sm: "none" },
+              }}
+            >
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box>
+              <IconBtn
+                title={"Search"}
+                icon={<SearchIcon />}
+                onClick={openSearchDialog}
+              />
+              <IconBtn
+                title={"New Group"}
+                icon={<AddIcon />}
+                onClick={openNewGroup}
+              />
+              <IconBtn
+                title={"Manage Groups"}
+                icon={<GroupIcon />}
+                onClick={navigateToGroup}
+              />
+              <IconBtn
+                title={"Logout"}
+                icon={<LogoutIcon />}
+                onClick={logoutHandler}
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
 };
 
+const IconBtn = ({ title, icon, onClick }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onClick}>
+        {icon}
+      </IconButton>
+    </Tooltip>
+  );
+};
 export default Header;
