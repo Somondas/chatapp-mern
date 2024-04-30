0b44795948f49a47f132ef3625bc2fd3acc036b2
Somondas <somondas067@gmail.com>
2024-04-30 18:02:32 +0530

    Profile Section Finished

--- client/src/components/specific/Profile.jsx
+++ client/src/components/specific/Profile.jsx
@@ -1,14 +1,58 @@
-import { Stack } from "@mui/material";
+import { Avatar, Stack, Typography } from "@mui/material";
 import React from "react";
-
+import {
+  Face as FaceIcon,
+  AlternateEmail as UserNameIcon,
+  CalendarMonth as CalendarIcon,
+} from "@mui/icons-material";
+import moment from "moment";
 const Profile = () => {
   return (
     <Stack direction={"column"} spacing={"2rem"} alignItems={"center"}>
-      <ProfileCard />
+      <Avatar
+        sx={{
+          width: 200,
+          height: 200,
+          objectFit: "contain",
+          marginBottom: "1rem",
+          border: "5px solid white",
+        }}
+      />
+      <ProfileCard
+        heading="Bio"
+        text="Aut excepturi deserunt 33 Quis molestias est corrupti consequatur sed debitis corporis et tempore "
+      />
+      <ProfileCard
+        heading="UserName"
+        text="_somondas_"
+        Icon={<UserNameIcon />}
+      />
+      <ProfileCard heading="Name" text="Somon Das" Icon={<FaceIcon />} />
+      <ProfileCard
+        heading="Joined"
+        text={moment("2023-11-02T18:30:00.000Z").fromNow()}
+        Icon={<CalendarIcon />}
+      />
     </Stack>
   );
 };
 
-const ProfileCard = () => <div>ProfileCard</div>;
+const ProfileCard = ({ text, Icon, heading }) => (
+  <Stack
+    direction="row"
+    alignItems={"center"}
+    spacing={"1rem"}
+    color={"white"}
+    textAlign={"center"}
+  >
+    {Icon && Icon}
+    <Stack>
+      <Typography variant="body1"> {text}</Typography>
+      <Typography color={"gray"} variant="caption">
+        {heading}
+      </Typography>
+    </Stack>
+  </Stack>
+);
 
 export default Profile;
