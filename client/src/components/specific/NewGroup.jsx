6f4963f8017bcd464d48ef50ae9b7695ac0b75bd
Somondas <somondas067@gmail.com>
2024-05-01 16:33:14 +0530

    Working on New Group Dialog Box

--- client/src/components/specific/NewGroup.jsx
+++ client/src/components/specific/NewGroup.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { memo } from "react";
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogTitle,
+  ListItem,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import UserItem from "../shared/UserItem";
+import { sampleUsers } from "../../constants/sampleData";
+import { useInputValidation } from "6pp";
 
 const NewGroupDialog = () => {
-  return <div>NewGroupDialog</div>;
+  const groupName = useInputValidation("");
+
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedMembers, setSelectedMembers] = useState([]);
+
+  const selectMemberHandler = () => {
+    console.log("selectMemberHabldler");
+  };
+  const submitHandler = () => {
+    console.log("submitHandler");
+  };
+  return (
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "2rem" }} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>New Group</DialogTitle>
+        <TextField
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+        />
+
+        <Typography variant="h6" pt={"1rem"}>
+          Members
+        </Typography>
+
+        <Stack pt={"1rem"}>
+          {sampleUsers.map((user) => (
+            <UserItem
+              key={user._id}
+              user={user}
+              handler={selectMemberHandler}
+            />
+          ))}
+        </Stack>
+        <Stack direction={"row"} pt={".5rem"} justifyContent={"space-around"}>
+          <Button variant="text" color={"error"}>
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={submitHanlder}>
+            Create
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
 };
 
 export default NewGroupDialog;
