1037b1894e2970b5ec575c216c10e8f5c125fb2b
Somondas <somondas067@gmail.com>
2024-05-02 17:33:50 +0530

    Working on Group page

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,7 +1,58 @@
+import { KeyboardBackspace as KeyboardBackspaceIcon } from "@mui/icons-material";
+import { Grid, IconButton, Tooltip } from "@mui/material";
 import React from "react";
 
 const Groups = () => {
-  return <div>Groups</div>;
+  const IconBtns = (
+    <>
+      <Tooltip title="Back">
+        <IconButton
+          sx={{
+            position: "absolute",
+            left: "2rem",
+            top: "2rem",
+            bgcolor: "rgba(0,0,0,0.9)",
+            color: "white",
+            "&:hover": {
+              bgcolor: "rgba(0,0,0,0.7)",
+            },
+          }}
+        >
+          <KeyboardBackspaceIcon />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+  return (
+    <>
+      <Grid container height={"100vh"}>
+        <Grid
+          item
+          sx={{
+            display: { xs: "none", sm: "block" },
+          }}
+          sm={4}
+          bgcolor={"bisque"}
+        >
+          Groups
+        </Grid>
+        <Grid
+          item
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            padding: "1rem 3rem",
+            position: "relative",
+          }}
+          xs={12}
+          sm={8}
+        >
+          {IconBtns}
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Groups;
