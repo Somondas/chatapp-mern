322ee122e2382c476ebc71b348f3ab7970b9357c
Somondas <somondas067@gmail.com>
2024-05-02 20:05:34 +0530

    Working on Group page

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,12 +1,53 @@
-import { KeyboardBackspace as KeyboardBackspaceIcon } from "@mui/icons-material";
-import { Grid, IconButton, Tooltip } from "@mui/material";
-import React from "react";
+import {
+  KeyboardBackspace as KeyboardBackspaceIcon,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
+import {
+  Box,
+  Drawer,
+  Grid,
+  IconButton,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Groups = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const navigateBack = () => {
+    navigate("/");
+  };
+  const handleMobile = () => {
+    // navigate("/mobile");
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+  const handleMobileClose = () => {
+    setIsMobileMenuOpen(false);
+  };
   const IconBtns = (
     <>
+      <Box
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+            position: "fixed",
+            right: "1rem",
+            top: "1rem",
+          },
+        }}
+      >
+        <IconButton onClick={handleMobile}>
+          <MenuIcon />
+        </IconButton>
+      </Box>
       <Tooltip title="Back">
         <IconButton
+          onClick={navigateBack}
           sx={{
             position: "absolute",
             left: "2rem",
@@ -50,9 +91,27 @@ const Groups = () => {
         >
           {IconBtns}
         </Grid>
+        <Drawer
+          sx={{
+            display: { xs: "block", sm: "none" },
+          }}
+          open={isMobileMenuOpen}
+          onClose={handleMobileClose}
+        >
+          Group List
+        </Drawer>
       </Grid>
     </>
   );
 };
 
+const GroupsList = ({ w = "100%", myGroups = [], chatId }) => (
+  <Stack>
+    {myGroups.length > 0 ? (
+      myGroups.map((group) => {})
+    ) : (
+      <Typography>No Groups</Typography>
+    )}
+  </Stack>
+);
 export default Groups;
