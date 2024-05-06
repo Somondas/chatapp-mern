9a571b99422206f3f979a7d60217d646faa6bf07
Somondas <somondas067@gmail.com>
2024-05-06 17:39:43 +0530

    Working on Dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,155 @@
 import React from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import {
+  Container,
+  Paper,
+  Stack,
+  Button,
+  Typography,
+  Box,
+} from "@mui/material";
+import {
+  AdminPanelSettings as AdminPanelSettingsIcon,
+  Group as GroupIcon,
+  Notifications as NotificationsIcon,
+  Person as PersonIcon,
+  Message as MessageIcon,
+} from "@mui/icons-material";
+import moment from "moment";
+import {
+  CurveButton,
+  SearchField,
+} from "../../components/styles/StyledComponents";
 
 const Dashboard = () => {
-  return <AdminLayout>Dashoard</AdminLayout>;
+  // >> Appbar Component--------------------------------------
+  const Appbar = (
+    <Paper
+      elevation={3}
+      sx={{
+        padding: "2rem",
+        margin: "2rem 0",
+        borderRadius: "1rem",
+      }}
+    >
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <AdminPanelSettingsIcon sx={{ fontSize: "3rem" }} />
+        <SearchField />
+        <CurveButton>Search</CurveButton>
+        <Box sx={{ flexGrow: 1 }} />
+        <Typography
+          variant=""
+          display={{
+            xs: "none",
+            lg: "block",
+          }}
+          color={"rgba(0, 0, 0, 0.7)"}
+          textAlign={"center"}
+        >
+          {moment().format("dddd, D MMMM YYYY")}
+        </Typography>
+        <NotificationsIcon />
+      </Stack>
+    </Paper>
+  );
+  // >> Widgets Component--------------------------------------
+  const Widgets = (
+    <Stack
+      direction={{
+        xs: "column",
+        sm: "row",
+      }}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      margin={"2rem 0"}
+      spacing={"2rem"}
+    >
+      <Widget title={"Users"} value={34} Icon={<GroupIcon />} />
+      <Widget title={"Chats"} value={34} Icon={<PersonIcon />} />
+      <Widget title={"Messages"} value={324} Icon={<MessageIcon />} />
+    </Stack>
+  );
+  return (
+    <AdminLayout>
+      <Container component={"main"}>
+        {Appbar}
+
+        <Stack direction={"row"} spacing={"2rem"} flexWrap={"wrap"}>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "2rem 3.5rem",
+              borderRadius: "1rem",
+              width: "100%",
+              maxWidth: "45rem",
+              height: "25rem",
+            }}
+          >
+            <Typography margin={"2rem 0"} variant="h4">
+              Last Messages
+            </Typography>
+            {"Caht"}
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "1rem",
+              borderRadius: "1rem",
+              width: "100%",
+              width: { xs: "100%", sm: "50%" },
+              maxWidth: "25rem",
+              position: "relative",
+              display: "flex",
+              justifyContent: "center",
+              height: "25rem",
+            }}
+          >
+            {"doufaskldjf"}
+            <Stack
+              position={"absolute"}
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              spacing={".5rem"}
+              width={"100%"}
+              height={"100%"}
+            >
+              <GroupIcon />
+              <Typography variant="">Vs</Typography>
+              <PersonIcon />
+            </Stack>
+          </Paper>
+        </Stack>
+        {Widgets}
+      </Container>
+    </AdminLayout>
+  );
 };
+// >> Widget Component-----------------------------------------
+const Widget = ({ title, value, Icon }) => (
+  <Paper
+    sx={{
+      padding: "2rem",
+      margin: "2rem 0",
+      borderRadius: "1rem",
+      width: "20rem",
+    }}
+  >
+    <Stack alignContent={"center"} spacing={"1rem"}>
+      <Typography variant="h5">{title}</Typography>
+      <Stack>{Icon}</Stack>
+      <Typography
+        sx={{
+          color: "rgba(0, 0, 0, 0.7)",
+          borderRadius: "50%",
+          border: "5px solid rgba(0, 0, 0, 0.9)",
+        }}
+        variant=""
+      >
+        {value}
+      </Typography>
+    </Stack>
+  </Paper>
+);
 
 export default Dashboard;
