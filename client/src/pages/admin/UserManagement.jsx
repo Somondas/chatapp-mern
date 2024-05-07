8f3c8ffb7323934d99196767e7f533cb965fb602
Somondas <somondas067@gmail.com>
2024-05-07 20:20:07 +0530

    Table finished, Now working on User Mangement

--- client/src/pages/admin/UserManagement.jsx
+++ client/src/pages/admin/UserManagement.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import Table from "../../components/shared/Table";
+import { Avatar } from "@mui/material";
 
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 200,
+    renderCell: (params) => (
+      <Avatar alt={params.row.name} src={params.row.avatar} />
+    ),
+  },
+  {
+    field: "name",
+    headerName: "Name",
+    headerClassName: "table-header",
+
+    width: 200,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "friends",
+    headerName: "Friends",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "groups",
+    headerName: "Groups",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  // {
+  //   field: "createdAt",
+  //   headerName: "Created At",
+  //   headerClassName: "table-header",
+  //   width: 200,
+  //   renderCell: (params) => (
+  //     <Avatar alt={params.row.name} src={params.row.avatar} />
+  //   ),
+  // },
+  // {
+  //   field: "updatedAt",
+  //   headerName: "Updated At",
+  //   headerClassName: "table-header",
+  //   width: 200,
+  // },
+];
 const UserManagement = () => {
+  const [rows, setRows] = useState([]);
   return (
     <AdminLayout>
-      <div>User</div>
+      <Table heading={"All Users"} columns={columns} rows={rows} />
     </AdminLayout>
   );
 };
