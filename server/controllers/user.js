5b83e67bc54dd70e417ff789ce7467941599a00b
Somondas <somondas067@gmail.com>
2024-05-27 19:39:55 +0530

    Working on user controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,10 @@
+// >> Create a user and save it to the database and also save cookie
+
+const newUser = (req, res) => {
+  res.send("Heloa");
+};
+const login = (req, res) => {
+  res.send("Heloa");
+};
+
+export { login, newUser };
