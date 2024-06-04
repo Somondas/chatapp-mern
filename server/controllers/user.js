4efec926ecbd7de180e9fe918a810b777764337b
Somondas <somondas067@gmail.com>
2024-06-04 14:08:41 +0530

    GetMyProfile Controller, done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -41,9 +41,10 @@ const login = TryCatch(async (req, res, next) => {
 // >> Get My Profile Controller----------------------------
 // const getMyProfile = TryCatch(async (req, res, next) => {});
 const getMyProfile = async (req, res) => {
+  const user = await User.findById(req.user);
   res.status(200).json({
     success: true,
-    data: "req.user",
+    user,
   });
 };
 
