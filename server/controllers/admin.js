e9bb9030c78833d5650abdd3a84e4d5be96e9667
Somondas <somondas067@gmail.com>
2024-06-24 14:28:55 +0530

    adminLogin controller, done

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -32,7 +32,6 @@ const allUsers = TryCatch(async (req, res) => {
 });
 
 // >> Get All Chats Controller--------------------------------
-
 const allChats = TryCatch(async (req, res) => {
   const chats = await Chat.find({})
     .populate("members", "name avatar")
@@ -143,5 +142,27 @@ const getDashboardStats = TryCatch(async (req, res) => {
   });
 });
 
+// >> Login Controller----------------------------------------
+const adminLogin = TryCatch(async (req, res, next) => {
+  const { secretKey } = req.body;
+
+  const isMatched = secretKey === adminSecretKey;
+
+  if (!isMatched) return next(new ErrorHandler("Invalid Admin Key", 401));
+
+  const token = jwt.sign(secretKey, process.env.JWT_SECRET);
+
+  return res
+    .status(200)
+    .cookie("chattu-admin-token", token, {
+      ...cookieOptions,
+      maxAge: 1000 * 60 * 15,
+    })
+    .json({
+      success: true,
+      message: "Authenticated Successfully, Welcome BOSS",
+    });
+});
+
 // -> All Exports==============================================
-export { allUsers, allChats, allMessages, getDashboardStats };
+export { allUsers, allChats, allMessages, getDashboardStats, adminLogin };
