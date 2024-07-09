b20609971916a1e91880bef0ab4eb264ebd119eb
Somondas <somondas067@gmail.com>
2024-07-09 15:27:33 +0530

    hook updates

--- client/src/hooks/hook.jsx
+++ client/src/hooks/hook.jsx
@@ -17,7 +17,29 @@ const useAsyncMutation = (mutationHook) => {
 
   const [mutate] = mutationHook();
 
-  const executeMutation = async (toastMessage, ...args) => {};
+  const executeMutation = async (toastMessage, ...args) => {
+    setIsLoading(true);
+    const toastId = toast.loading(toastMessage || "Updating data...");
+
+    try {
+      const res = await mutate(...args);
+      if (res.data) {
+        toast.success(res.data.message || "Updated data successfully", {
+          id: toastId,
+        });
+        setData(res.data);
+      } else {
+        toast.error(res?.error?.data?.message || "Something went wrong", {
+          id: toastId,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong", { id: toastId });
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return executeMutation, isLoading, data;
 };
