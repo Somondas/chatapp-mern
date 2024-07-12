01a29b1eaa7d3ec995e18748875627fb380f7591
Somondas <somondas067@gmail.com>
2024-07-12 15:23:11 +0530

    updates

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,21 +1,14 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useMemo, useContext } from "react";
 import io from "socket.io-client";
 import { server } from "./constants/config";
 
 const SocketContext = createContext();
 
-const getSocket = () => {
-  return useContext(SocketContext);
-};
+const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }) => {
-  const socket = useMemo(
-    () =>
-      io(server, {
-        withCredentials: true,
-      }),
-    []
-  );
+  const socket = useMemo(() => io(server, { withCredentials: true }), []);
+
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
