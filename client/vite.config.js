38c9ee3aeff170f6ec7992971d72086d249b71c7
Somondas <somondas067@gmail.com>
2024-07-12 19:04:20 +0530

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+// import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["js-big-decimal"],
-  },
+
+  // optimizeDeps: {
+  //   exclude: ["js-big-decimal"],
+  // },
 });
