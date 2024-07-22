95f827debcf76e8df6a9b22f9eec416b9977752b
Somondas <somondas067@gmail.com>
2024-07-22 19:56:28 +0530

    FileMenu UI, done

--- client/src/components/dialogs/FileMenu.jsx
+++ client/src/components/dialogs/FileMenu.jsx
@@ -2,7 +2,12 @@ import { ListItemText, Menu, MenuItem, MenuList, Tooltip } from "@mui/material";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsFileMenu } from "../../redux/reducers/misc";
-import { Image as ImageIcon } from "@mui/icons-material";
+import {
+  AudioFile as AudioFileIcon,
+  Image as ImageIcon,
+  UploadFile as UploadFileIcon,
+  VideoFile as VideoFileIcon,
+} from "@mui/icons-material";
 
 const FileMenu = ({ anchorEl }) => {
   const { isFileMenu } = useSelector((state) => state.misc);
@@ -16,6 +21,7 @@ const FileMenu = ({ anchorEl }) => {
           width: "10rem",
         }}
       >
+        {/* First ======================= */}
         <MenuList>
           <MenuItem>
             <Tooltip title="Image">
@@ -25,10 +31,65 @@ const FileMenu = ({ anchorEl }) => {
             <input
               type="file"
               multiple
+              accept="video/mp4, video/webm, video/ogg"
+              style={{
+                display: "none",
+              }}
+              onChange={(e) => fileChangeHandler(e, "Videos")}
+            />
+          </MenuItem>
+        </MenuList>
+        {/* Second ======================= */}
+        <MenuList>
+          <MenuItem>
+            <Tooltip title="Audio">
+              <AudioFileIcon />
+            </Tooltip>
+            <ListItemText style={{ marginLeft: ".5rem" }}>Audio</ListItemText>
+            <input
+              type="file"
+              multiple
               accept="image/png, image/jpeg, image/gif"
               style={{
                 display: "none",
               }}
+              onChange={(e) => fileChangeHandler(e, "Images")}
+            />
+          </MenuItem>
+        </MenuList>
+        {/* Third ======================= */}
+        <MenuList>
+          <MenuItem>
+            <Tooltip title="Video">
+              <VideoFileIcon />
+            </Tooltip>
+            <ListItemText style={{ marginLeft: ".5rem" }}>Video</ListItemText>
+            <input
+              type="file"
+              multiple
+              accept="image/png, image/jpeg, image/gif"
+              style={{
+                display: "none",
+              }}
+              onChange={(e) => fileChangeHandler(e, "Videos")}
+            />
+          </MenuItem>
+        </MenuList>
+        {/* Fourth ======================= */}
+        <MenuList>
+          <MenuItem>
+            <Tooltip title="File">
+              <UploadFileIcon />
+            </Tooltip>
+            <ListItemText style={{ marginLeft: ".5rem" }}>File</ListItemText>
+            <input
+              type="file"
+              multiple
+              accept="*"
+              style={{
+                display: "none",
+              }}
+              onChange={(e) => fileChangeHandler(e, "Files")}
             />
           </MenuItem>
         </MenuList>
