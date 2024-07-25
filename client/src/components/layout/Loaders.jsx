2c78e962dcabd41efa9a3d22f5be6d64045fd234
Somondas <somondas067@gmail.com>
2024-07-25 12:37:21 +0530

    Updates

--- client/src/components/layout/Loaders.jsx
+++ client/src/components/layout/Loaders.jsx
@@ -1,8 +1,8 @@
-import { SevenKOutlined } from "@mui/icons-material";
 import { Grid, Skeleton, Stack } from "@mui/material";
 import React from "react";
+import { BouncingSkeleton } from "../styles/StyledComponents";
 
-export const LayoutLoader = () => {
+const LayoutLoader = () => {
   return (
     <Grid container height={"calc(100vh - 4rem)"} spacing={"1rem"}>
       <Grid
@@ -16,21 +16,14 @@ export const LayoutLoader = () => {
       >
         <Skeleton variant="rectangular" height={"100vh"} />
       </Grid>
-      <Grid
-        item
-        xs={12}
-        md={5}
-        sm={8}
-        lg={6}
-        height={"100%"}
-        // bgcolor={"primary.main"}
-      >
+      <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
         <Stack spacing={"1rem"}>
-          {Array.from({ length: 10 }, (_, i) => (
-            <Skeleton variant="rectangular" key={i} height={"5rem"} />
+          {Array.from({ length: 10 }).map((_, index) => (
+            <Skeleton key={index} variant="rounded" height={"5rem"} />
           ))}
         </Stack>
       </Grid>
+
       <Grid
         item
         md={4}
@@ -45,3 +38,49 @@ export const LayoutLoader = () => {
     </Grid>
   );
 };
+
+const TypingLoader = () => {
+  return (
+    <Stack
+      spacing={"0.5rem"}
+      direction={"row"}
+      padding={"0.5rem"}
+      justifyContent={"center"}
+    >
+      <BouncingSkeleton
+        variant="circular"
+        width={15}
+        height={15}
+        style={{
+          animationDelay: "0.1s",
+        }}
+      />
+      <BouncingSkeleton
+        variant="circular"
+        width={15}
+        height={15}
+        style={{
+          animationDelay: "0.2s",
+        }}
+      />
+      <BouncingSkeleton
+        variant="circular"
+        width={15}
+        height={15}
+        style={{
+          animationDelay: "0.4s",
+        }}
+      />
+      <BouncingSkeleton
+        variant="circular"
+        width={15}
+        height={15}
+        style={{
+          animationDelay: "0.6s",
+        }}
+      />
+    </Stack>
+  );
+};
+
+export { TypingLoader, LayoutLoader };
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -17,7 +17,7 @@ import FileMenu from "../components/dialogs/FileMenu";
 import { sampleMessage } from "../constants/sampleData";
 import MessageComponent from "../components/shared/MessageComponent";
 import { getSocket } from "../socket";
-import { NEW_MESSAGE, START_TYPING } from "../constants/events";
+import { NEW_MESSAGE, START_TYPING, STOP_TYPING } from "../constants/events";
 import { useChatDetailsQuery, useGetMessagesQuery } from "../redux/api/api";
 import { useErrors, useSocketEvents } from "../hooks/hook";
 import { useInfiniteScrollTop } from "6pp";
@@ -113,6 +113,7 @@ const Chat = ({ chatId, user }) => {
       if (data.chatId !== chatId) return;
       // console.log(data);
       console.log("typing", data);
+      setUserTyping(true);
     },
     [chatId]
   );
