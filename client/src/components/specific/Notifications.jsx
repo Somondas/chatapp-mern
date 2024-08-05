485daa4abcf8c0694697e63c9d9524e260917e2a
Somondas <somondas067@gmail.com>
2024-08-05 16:20:48 +0530

    updates

--- client/src/components/specific/Notifications.jsx
+++ client/src/components/specific/Notifications.jsx
@@ -1,4 +1,3 @@
-import React, { memo } from "react";
 import {
   Avatar,
   Button,
@@ -9,67 +8,53 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import { sampleNotifications } from "../../constants/sampleData";
-import toast from "react-hot-toast";
-import { useGetNotificationsQuery } from "../../redux/api/api";
-import { useAcceptFriendRequestMutation } from "../../redux/api/api";
-import { useErrors } from "../../hooks/hook";
+import React, { memo } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useAsyncMutation, useErrors } from "../../hooks/hook";
+import {
+  useAcceptFriendRequestMutation,
+  useGetNotificationsQuery,
+} from "../../redux/api/api";
 import { setIsNotification } from "../../redux/reducers/misc";
 
-// |========================================================-=-
 const Notifications = () => {
-  const dispatch = useDispatch();
   const { isNotification } = useSelector((state) => state.misc);
+
+  const dispatch = useDispatch();
+
   const { isLoading, data, error, isError } = useGetNotificationsQuery();
 
+  const [acceptRequest] = useAsyncMutation(useAcceptFriendRequestMutation);
+
+  const friendRequestHandler = async ({ _id, accept }) => {
+    dispatch(setIsNotification(false));
+    await acceptRequest("Accepting...", { requestId: _id, accept });
+  };
+
+  const closeHandler = () => dispatch(setIsNotification(false));
+
   useErrors([{ error, isError }]);
-
-  const [acceptRequest] = useAcceptFriendRequestMutation();
-  const frienRequestHandler = async ({ _id, accept }) => {
-    console.log(_id);
-    console.log(accept);
-    dispatch(setIsNotification(false));
-
-    try {
-      const res = await acceptRequest({ requestId: _id, accept });
-      console.log(res);
-      if (res.data?.success) {
-        console.log("Use SocketHere");
-        toast.success(res.data.message);
-      } else {
-        // toast.error(res.data?.message || "Something went wrong");
-        console.log("Something went wrong");
-      }
-    } catch (error) {
-      // toast.error("Something went wrong");
-      console.log(error);
-    }
-  };
-
-  const closeHandler = () => {
-    dispatch(setIsNotification(false));
-  };
 
   return (
     <Dialog open={isNotification} onClose={closeHandler}>
       <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"25rem"}>
         <DialogTitle>Notifications</DialogTitle>
+
         {isLoading ? (
           <Skeleton />
         ) : (
           <>
             {data?.allRequest.length > 0 ? (
-              data?.allRequest.map((i) => (
+              data?.allRequest?.map(({ sender, _id }) => (
                 <NotificationItem
-                  sender={i.sender}
-                  _id={i._id}
-                  handler={frienRequestHandler}
-                  key={i._id}
+                  sender={sender}
+                  _id={_id}
+                  handler={friendRequestHandler}
+                  key={_id}
                 />
               ))
             ) : (
-              <Typography textAlign={"center"}>0 Notification</Typography>
+              <Typography textAlign={"center"}>0 notifications</Typography>
             )}
           </>
         )}
@@ -89,10 +74,11 @@ const NotificationItem = memo(({ sender, _id, handler }) => {
         width={"100%"}
       >
         <Avatar />
+
         <Typography
           variant="body1"
           sx={{
-            flexGrow: 1,
+            flexGlow: 1,
             display: "-webkit-box",
             WebkitLineClamp: 1,
             WebkitBoxOrient: "vertical",
@@ -101,8 +87,9 @@ const NotificationItem = memo(({ sender, _id, handler }) => {
             width: "100%",
           }}
         >
-          {`${name} send you a friend request`}
+          {`${name} sent you a friend request.`}
         </Typography>
+
         <Stack
           direction={{
             xs: "column",
@@ -118,4 +105,5 @@ const NotificationItem = memo(({ sender, _id, handler }) => {
     </ListItem>
   );
 });
+
 export default Notifications;
